8f9e235e5591afedec8bcc480e280ccd77ff3d2b
Takmin <jtm0609@naver.com>
2024-12-20 20:25:03 +0900

    [fix] Kakao Map V2 Migration 대응

--- presentation/src/main/java/com/jtmcompany/parkingapplication/application/ParkApplication.kt
+++ presentation/src/main/java/com/jtmcompany/parkingapplication/application/ParkApplication.kt
@@ -3,6 +3,7 @@ package com.jtmcompany.parkingapplication.application
 import android.app.Application
 import com.jtmcompany.parkingapplication.R
 import com.kakao.sdk.common.KakaoSdk
+import com.kakao.vectormap.KakaoMapSdk
 import com.skt.Tmap.TMapTapi
 import dagger.hilt.android.HiltAndroidApp
 
@@ -15,6 +16,7 @@ class ParkApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         KakaoSdk.init(this, getString(R.string.kakao_native_app_key))
+        KakaoMapSdk.init(this, getString(R.string.kakao_native_app_key))
         TMapTapi(this).setSKTMapAuthentication(getString(R.string.tmap_native_app_key))
     }
 }
--- presentation/src/main/java/com/jtmcompany/parkingapplication/ui/fragment/ParkLocationFragment.kt
+++ presentation/src/main/java/com/jtmcompany/parkingapplication/ui/fragment/ParkLocationFragment.kt
@@ -1,21 +1,38 @@
 package com.jtmcompany.parkingapplication.ui.fragment
 
+import android.annotation.SuppressLint
+import android.location.Location
+import android.os.Looper
+import android.util.Log
 import androidx.fragment.app.viewModels
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import com.jtmcompany.parkingapplication.R
 import com.jtmcompany.parkingapplication.base.BaseFragment
 import com.jtmcompany.parkingapplication.databinding.FragmentParkLocationBinding
 import com.jtmcompany.parkingapplication.ui.viewmodel.ParkLocationViewModel
+import com.kakao.vectormap.KakaoMap
+import com.kakao.vectormap.KakaoMapReadyCallback
+import com.kakao.vectormap.LatLng
+import com.kakao.vectormap.MapLifeCycleCallback
+import com.kakao.vectormap.label.Label
+import com.kakao.vectormap.label.LabelOptions
+import com.kakao.vectormap.label.LabelStyle
+import com.kakao.vectormap.label.LabelStyles
 import dagger.hilt.android.AndroidEntryPoint
-import net.daum.mf.map.api.MapPoint
-import net.daum.mf.map.api.MapView
+import java.lang.Exception
 
 @AndroidEntryPoint
 class ParkLocationFragment :
-    BaseFragment<FragmentParkLocationBinding, ParkLocationViewModel>(R.layout.fragment_park_location),
-    MapView.CurrentLocationEventListener {
+    BaseFragment<FragmentParkLocationBinding, ParkLocationViewModel>(R.layout.fragment_park_location) {
 
     override val viewModel: ParkLocationViewModel by viewModels()
-    private lateinit var mapView: MapView
+    private val fusedLocationProviderClient by lazy {LocationServices.getFusedLocationProviderClient(mContext)}
+    private lateinit var locationCallback : LocationCallback
+    private lateinit var kakaoMap : KakaoMap
 
     override fun setBindingVariable(binding: FragmentParkLocationBinding) {
         with(binding) {
@@ -29,43 +46,85 @@ class ParkLocationFragment :
         viewModel.requestParkInfo(1)
     }
 
+    private fun initMapView() {
+        binding.mapView.start(object : MapLifeCycleCallback() {
+            override fun onMapDestroy() {
+                Log.d("taek", "onMapDestroy")
+            }
+
+            override fun onMapError(exception: Exception?) {
+                Log.d("taek", "onMapError: $exception")
+            }
+
+        }, object : KakaoMapReadyCallback() {
+            override fun onMapReady(kakaoMap: KakaoMap) {
+                Log.d("taek", "onMapReady")
+                this@ParkLocationFragment.kakaoMap = kakaoMap
+                setLocationListener()
+            }
+        })
+    }
+
     override fun initObserver() {
         viewModel.clickedSearch.observe(viewLifecycleOwner) {
-            val action = ParkLocationFragmentDirections.actionParkLocationFragmentToParkSearchFragment(
-                viewModel.userLatitude.toFloat(),
-                viewModel.userLongitude.toFloat()
-            )
+            val action =
+                ParkLocationFragmentDirections.actionParkLocationFragmentToParkSearchFragment(
+                    viewModel.userLatitude.toFloat(),
+                    viewModel.userLongitude.toFloat()
+                )
             nextFragment(action)
         }
     }
 
-    private fun initMapView() {
-        mapView = MapView(mContext)
-        binding.mapView.addView(mapView)
-        mapView.setCurrentLocationEventListener(this)
-
-        //현재 위치 표시
-        mapView.currentLocationTrackingMode =
-            MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading
-    }
-
-    //현 위치 파악 (카카오 SDK)
-    override fun onCurrentLocationUpdate(view: MapView?, currentLocation: MapPoint?, p2: Float) {
-        currentLocation?.let { mapPoint ->
-            viewModel.setUserLocation(
-                mapPoint.mapPointGeoCoord.latitude,
-                mapPoint.mapPointGeoCoord.longitude
-            )
+    @SuppressLint("MissingPermission")
+    private fun setLocationListener() {
+        fusedLocationProviderClient.lastLocation.addOnSuccessListener {location ->
+            updateLocation(location)
         }
+
+        val locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 1000).build()
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(location: LocationResult) {
+                super.onLocationResult(location)
+                updateLocation(location.lastLocation)
+            }
+        }
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.getMainLooper()
+        )
     }
 
-    override fun onCurrentLocationDeviceHeadingUpdate(p0: MapView?, p1: Float) {
+    private fun updateLocation(location: Location?) {
+        val latitude = location?.latitude ?: 37.0
+        val longitude = location?.longitude ?: 127.0
+        val label = kakaoCreateLabel(latitude, longitude)
+
+        val trackingManager = kakaoMap.trackingManager
+        trackingManager?.startTracking(label)
+        trackingManager?.setTrackingRotation(true)
+        kakaoMap.compass?.show()
+
+        viewModel.setUserLocation(latitude, longitude)
     }
 
-    override fun onCurrentLocationUpdateFailed(p0: MapView?) {
+    private fun kakaoCreateLabel(latitude: Double, longitude: Double) : Label? {
+        val style =
+            kakaoMap.labelManager?.addLabelStyles(LabelStyles.from(LabelStyle.from(R.drawable.marker)))
+        val options = LabelOptions.from(
+            LatLng.from(
+                latitude,
+                longitude
+            )
+        ).setStyles(style)
+        val layer = kakaoMap.labelManager?.layer
+        layer?.removeAll()
+        return layer?.addLabel(options)
     }
 
-    override fun onCurrentLocationUpdateCancelled(p0: MapView?) {
+    override fun onDestroyView() {
+        super.onDestroyView()
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
     }
-
 }
